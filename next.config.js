b642f8d6260f1d49919b096b066c77b26ee826d2
Afonso Henrique Anastacio Calabria <ahacalabria@gmail.com>
2021-06-10 10:03:00 -0300

    first page edited

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withSass = require("@zeit/next-sass");
+module.exports = withSass();
+
+// If you already have some config
+
+module.exports = withSass({
+  target: 'serverless',
+  env: {
+    JWT_SECRET: process.env.JWT_SECRET
+  }
+});
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+// import styles from './index.module.css';
 
 export default function Home() {
   return (
@@ -10,39 +11,43 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Bem-vindo à <a href="https://nextjs.org">Connect Com!!!</a>
+          Bem-vindo à <a href="https://nextjs.org">Connect Com!</a>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Conectando você e seus fornecedores.
         </p>
+
+        <form>
+          <input type="text" className="search-input" placeholder="Qual sua necessidade?" /><button className="btn">buscar</button>
+        </form>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Quero me cadastrar &rarr;</h3>
+            <p>Cadastra-se para acessar todos os recursos.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Conheça mais sobre &rarr;</h3>
+            <p>A ConnectCom é StartUp, é inovação.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Divulgar meus produtos &rarr;</h3>
+            <p>Alcance seus clientes da melhor forma.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>FAQ &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Conheça as principais dúvidas ao utilizar a ConnectCom.
             </p>
           </a>
         </div>
@@ -54,8 +59,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by{' '} ConnectCom
+          {/* <img src="/vercel.svg" alt="Vercel Logo" className="logo" /> */}
         </a>
       </footer>
 
@@ -180,6 +185,25 @@ export default function Home() {
 
         .logo {
           height: 1em;
+        }
+
+        .search-input {
+          padding: 8px 10px;
+          height: 40px;
+          width: 700px;
+          border: 2px solid #eaeaea;
+          // box-shadow: 0 0 5px 5px #888;
+        }
+        .btn{
+          padding: 8px 10px;
+          height: 40px;
+          background-color: #0070f3;
+          border: none;
+          outline: none;
+          color: #fff;
+          border-radius: 0px 4px 4px 0px;
+          font-weight: bold;
+          cursor: pointer;
         }
 
         @media (max-width: 600px) {
